c84a08bafa116ec15ce611c75faa50310a7a1dff
tainanpenning <tainanhartwigpenning@gmail.com>
2025-04-08 12:41:53 -0300

    criando todo-list em um arquivo

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,97 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+    const estado = reactive({
+        filtro: "todas",
+        tarefaTemp: "",
+        tarefas: [
+            {
+                titulo: "Estudar ES6",
+                finalizada: false,
+            },
+            {
+                titulo: "Estudar SASS",
+                finalizada: false,
+            },
+            {
+                titulo: "Ir para a academia",
+                finalizada: true,
+            },
+        ]
+    })
+
+    const getTarefasPendentes = () => {
+        return estado.tarefas.filter(tarefa => !tarefa.finalizada)
+    }
+    
+    const getTarefasFinalizadas = () => {
+        return estado.tarefas.filter(tarefa => tarefa.finalizada)
+    }
+
+    const getTarefasFiltradas = () => {
+        const { filtro } = estado
+
+        switch (filtro){
+            case "pendentes":
+                return getTarefasPendentes()
+            case "finalizadas":
+                return getTarefasFinalizadas()
+            default:
+                return estado.tarefas
+        }
+    }
+
+    const adicionaTarefa = () => {
+        const novaTarefa = {
+            titulo: estado.tarefaTemp,
+            finalizada: false,
+        }
+        estado.tarefas.push(novaTarefa)
+        estado.tarefaTemp = ""
+    }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="container">
+        <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+            <h1>Minhas Tarefas</h1>
+            <p v-if="getTarefasPendentes().length > 0">
+                Você possui {{ getTarefasPendentes().length }} tarefas pendentes
+            </p>
+            <p v-if="getTarefasPendentes().length === 0">
+                Você não possui tarefas pendentes
+            </p>
+        </header>
+        <form @submit.prevent="adicionaTarefa">
+            <div class="row">
+                <div class="col">
+                    <input required class="form-control" type="text" placeholder="Digite a descrição da tarefa" :value="estado.tarefaTemp" @change="e => estado.tarefaTemp = e.target.value">
+                </div>
+                <div class="col-md-2">
+                    <button class="btn btn-primary" type="submit">Adicionar</button>
+                </div>
+                <div class="col-md-2">
+                    <select class="form-control" @change="e => estado.filtro = e.target.value">
+                        <option value="todas">Todas</option>
+                        <option value="pendentes">Pendentes</option>
+                        <option value="finalizadas">Finalizadas</option>
+                    </select>
+                </div>
+            </div>
+        </form>
+        <ul class="list-group mt-4">
+            <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
+                <input type="checkbox" @change="e => tarefa.finalizada = e.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo">
+                <label class="ms-3" :class="{ done: tarefa.finalizada }" :for="tarefa.titulo">
+                    {{ tarefa.titulo }}
+                </label>
+            </li>
+        </ul>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+    .done{
+        text-decoration: line-through;
+    }
 </style>
